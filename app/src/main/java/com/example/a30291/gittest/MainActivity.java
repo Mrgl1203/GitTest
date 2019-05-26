faec591af56fe63b4aab18fa2ad85562ee7b7ac7
Mrgl1203 <gl152217@163.com>
2019-05-26 15:10:01 +0800

    添加动画

--- app/src/main/java/com/example/a30291/gittest/MainActivity.java
+++ app/src/main/java/com/example/a30291/gittest/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.a30291.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         //xiugai
         //develop create
         //develop create2
+        tv = findViewById(R.id.tvText);
+    }
+
+
+    public void translate(View view) {
+        TranslateAnimation translateAnimation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 500,
+                Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF,500 );
+        translateAnimation.setDuration(1000);
+        tv.startAnimation(translateAnimation);
     }
 }
